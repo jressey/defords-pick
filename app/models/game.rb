6bb90211ac484edcb39aee962cea465735228b12
Joe Essey <joe.essey@salesloft.com>
2018-11-24 10:40:54 -0500

    nice convinience method for getting teams of a game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,12 @@
 class Game < ApplicationRecord
-  has_one :away_team, class_name: "Team", foreign_key: "away_team_id"
-  has_one :home_team, class_name: "Team", foreign_key: "home_team_id"
+  belongs_to :away_team, class_name: "Team", foreign_key: "away_team_id"
+  belongs_to :home_team, class_name: "Team", foreign_key: "home_team_id"
   belongs_to :sport
+
+  def teams
+    {
+      away_team: away_team,
+      home_team: home_team
+    }
+  end
 end
