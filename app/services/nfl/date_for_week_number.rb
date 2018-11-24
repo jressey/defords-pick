9182fdad112ce6568c2679577c92546efad10344
Joe Essey <joe.essey@salesloft.com>
2018-11-24 17:39:12 -0500

    move weekly cycle to wednesday instead of tuesday

--- app/services/nfl/date_for_week_number.rb
+++ app/services/nfl/date_for_week_number.rb
@@ -7,71 +7,71 @@ class NFL::DateForWeekNumber
   def self.dat
     {
     1 => {
-      start_day: Date.new(2018, 9, 4),
-      end_day: Date.new(2018, 9, 11)
+      start_day: Date.new(2018, 9, 5),
+      end_day: Date.new(2018, 9, 12)
     },
     2 => {
-      start_day: Date.new(2018, 9, 11),
-      end_day: Date.new(2018, 9, 18)
+      start_day: Date.new(2018, 9, 12),
+      end_day: Date.new(2018, 9, 19)
     },
     3 => {
-      start_day: Date.new(2018, 9, 18),
-      end_day: Date.new(2018, 9, 25)
+      start_day: Date.new(2018, 9, 19),
+      end_day: Date.new(2018, 9, 26)
     },
     4 => {
-      start_day: Date.new(2018, 9, 25),
-      end_day: Date.new(2018, 10,2)
+      start_day: Date.new(2018, 9, 26),
+      end_day: Date.new(2018, 10, 3)
     },
     5 => {
-      start_day: Date.new(2018, 10,2),
-      end_day: Date.new(2018, 10, 9)
+      start_day: Date.new(2018, 10, 3),
+      end_day: Date.new(2018, 10, 10)
     },
     6 => {
-      start_day: Date.new(2018, 10, 9),
-      end_day: Date.new(2018, 10, 16)
+      start_day: Date.new(2018, 10, 10),
+      end_day: Date.new(2018, 10, 17)
     },
     7 => {
-      start_day: Date.new(2018, 10, 16),
-      end_day: Date.new(2018, 10, 23)
+      start_day: Date.new(2018, 10, 17),
+      end_day: Date.new(2018, 10, 24)
     },
     8 => {
-      start_day: Date.new(2018, 10, 23),
-      end_day: Date.new(2018, 10, 30)
+      start_day: Date.new(2018, 10, 24),
+      end_day: Date.new(2018, 10, 31)
     },
     9 => {
-      start_day: Date.new(2018, 10, 30),
-      end_day: Date.new(2018, 11, 6)
+      start_day: Date.new(2018, 10, 31),
+      end_day: Date.new(2018, 11, 7)
     },
     10 => {
-      start_day: Date.new(2018, 11, 6),
-      end_day: Date.new(2018, 11, 13)
+      start_day: Date.new(2018, 11, 7),
+      end_day: Date.new(2018, 11, 14)
     },
     11 => {
-      start_day: Date.new(2018, 11, 13),
-      end_day: Date.new(2018, 11, 20)
+      start_day: Date.new(2018, 11, 14),
+      end_day: Date.new(2018, 11, 21)
     },
     12 => {
-      start_day: Date.new(2018, 11, 20),
-      end_day: Date.new(2018, 11, 27)
+      start_day: Date.new(2018, 11, 21),
+      end_day: Date.new(2018, 11, 28)
     },
     13 => {
-      start_day: Date.new(2018, 11, 27),
-      end_day: Date.new(2018, 12, 4)
+      start_day: Date.new(2018, 11, 28),
+      end_day: Date.new(2018, 12, 5)
     },
     14 => {
-      start_day: Date.new(2018, 12, 4),
-      end_day: Date.new(2018, 12, 11)
+      start_day: Date.new(2018, 12, 5),
+      end_day: Date.new(2018, 12, 12)
     },
     15 => {
-      start_day: Date.new(2018, 12, 11),
-      end_day: Date.new(2018, 9, 18)
+      start_day: Date.new(2018, 12, 12),
+      end_day: Date.new(2018, 9, 19)
     },
     16 => {
-      start_day: Date.new(2018, 9, 18),
-      end_day: Date.new(2018, 12, 25)
+      start_day: Date.new(2018, 9, 19),
+      end_day: Date.new(2018, 12, 26)
     },
     17 => {
-      start_day: Date.new(2018, 12, 25),
+      start_day: Date.new(2018, 12, 26),
       end_day: Date.new(2018, 1, 2)
     },
   }
--- app/services/nfl/week_number_for_date.rb
+++ app/services/nfl/week_number_for_date.rb
@@ -2,37 +2,37 @@ class NFL::WeekNumberForDate
   class << self
 
     def call(date)
-      if (Date.new(2018, 9, 4)..Date.new(2018, 9, 11)).include?(date)
+      if (Date.new(2018, 9, 5)..Date.new(2018, 9, 12)).include?(date)
         1
-      elsif (Date.new(2018, 9, 11)..Date.new(2018, 9, 18)).include?(date)
+      elsif (Date.new(2018, 9, 12)..Date.new(2018, 9, 19)).include?(date)
         2
-      elsif (Date.new(2018, 9, 18)..Date.new(2018, 9, 25)).include?(date)
+      elsif (Date.new(2018, 9, 19)..Date.new(2018, 9, 26)).include?(date)
         3
-      elsif (Date.new(2018, 9, 25)..Date.new(2018, 10,2)).include?(date)
+      el sif (Date.new(2018, 9, 26)..Date.new(2018, 10, 3)).include?(date)
         4
-      elsif (Date.new(2018, 10, 2)..Date.new(2018, 10, 9)).include?(date)
+      elsif (Date.new(2018, 10, 3)..Date.new(2018, 10, 10)).include?(date)
         5
-      elsif (Date.new(2018, 10, 9)..Date.new(2018, 10, 16)).include?(date)
+      elsif (Date.new(2018, 10, 10)..Date.new(2018, 10, 17)).include?(date)
         6
-      elsif (Date.new(2018, 10, 16)..Date.new(2018, 10, 23)).include?(date)
+      elsif (Date.new(2018, 10, 17)..Date.new(2018, 10, 24)).include?(date)
         7
-      elsif (Date.new(2018, 10, 23)..Date.new(2018, 10, 30)).include?(date)
+      elsif (Date.new(2018, 10, 24)..Date.new(2018, 10, 31)).include?(date)
         8
-      elsif (Date.new(2018, 10, 30)..Date.new(2018, 11, 6)).include?(date)
+      elsif (Date.new(2018, 10, 31)..Date.new(2018, 11, 7)).include?(date)
         9
-      elsif (Date.new(2018, 11, 6)..Date.new(2018, 11, 13)).include?(date)
+      elsif (Date.new(2018, 11, 7)..Date.new(2018, 11, 14)).include?(date)
         10
-      elsif (Date.new(2018, 11, 13)..Date.new(2018, 11, 20)).include?(date)
+      elsif (Date.new(2018, 11, 14)..Date.new(2018, 11, 21)).include?(date)
         11
-      elsif (Date.new(2018, 11, 20)..Date.new(2018, 11, 27)).include?(date)
+      elsif (Date.new(2018, 11, 21)..Date.new(2018, 11, 28)).include?(date)
         12
-      elsif (Date.new(2018, 11, 27)..Date.new(2018, 12, 4)).include?(date)
+      elsif (Date.new(2018, 11, 28)..Date.new(2018, 12, 5)).include?(date)
         13
-      elsif (Date.new(2018, 12, 4)..Date.new(2018, 12, 11)).include?(date)
+      elsif (Date.new(2018, 12, 5)..Date.new(2018, 12, 12)).include?(date)
         14
-      elsif (Date.new(2018, 12, 11)..Date.new(2018, 9, 18)).include?(date)
+      elsif (Date.new(2018, 12, 12)..Date.new(2018, 9, 19)).include?(date)
         15
-      elsif (Date.new(2018, 12, 18)..Date.new(2018, 12, 25)).include?(date)
+      elsif (Date.new(2018, 12, 19)..Date.new(2018, 12, 26)).include?(date)
         16
       else
         17
