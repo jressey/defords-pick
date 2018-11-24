dd4e04e40f63f7935e034049e56f559516c329f6
Joe Essey <joe.essey@salesloft.com>
2018-11-24 18:55:15 -0500

    migrations

--- config/initializers/data_loader.rb
+++ config/initializers/data_loader.rb
@@ -1,8 +1,8 @@
 if Rails.env == "production"
-  Sport.find_or_create_by(name: "Football", league_name: "NFL")
-  Sport.find_or_create_by(name: "Baseball", league_name: "MLB")
-  Sport.find_or_create_by(name: "Hockey", league_name: "NHL")
-  Sport.find_or_create_by(name: "Basketball", league_name: "NBA")
+  # Sport.find_or_create_by(name: "Football", league_name: "NFL")
+  # Sport.find_or_create_by(name: "Baseball", league_name: "MLB")
+  # Sport.find_or_create_by(name: "Hockey", league_name: "NHL")
+  # Sport.find_or_create_by(name: "Basketball", league_name: "NBA")
 
   # NFL::InboundStructureProcessor.call
   # NFL::InboundScheduleProcessor.call
