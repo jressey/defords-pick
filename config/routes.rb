23effe6a9bc4b007f3cef74a3a8287cc08b9d2f0
Joe Essey <joe.essey@salesloft.com>
2018-11-25 07:56:14 -0500

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   namespace :schedules do
     get "/nfl", to: "nfl#daily"
+    get "/nfl/game_of_the_week", to: "nfl#game_of_the_week"
     get "/nhl", to: "nhl#daily"
   end
+
+  root to: "static#home"
 
 end
