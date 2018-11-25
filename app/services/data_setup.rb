fda7b3860b19cf3829c72910f953bfa597620bbf
Joe Essey <joe.essey@salesloft.com>
2018-11-24 19:10:20 -0500

    add data setup service

---
+++ app/services/data_setup.rb
@@ -0,0 +1,15 @@
+class DataSetup
+
+  def call
+    Sport.find_or_create_by(name: "Football", league_name: "NFL")
+    Sport.find_or_create_by(name: "Baseball", league_name: "MLB")
+    Sport.find_or_create_by(name: "Hockey", league_name: "NHL")
+    Sport.find_or_create_by(name: "Basketball", league_name: "NBA")
+
+    NFL::InboundStructureProcessor.call
+    NFL::InboundScheduleProcessor.call
+
+    NHL::InboundStructureProcessor.call
+  end
+
+end
