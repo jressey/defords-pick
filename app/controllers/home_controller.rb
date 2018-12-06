44272d2509cd1858125e460867ff486be5fc80b1
Joe Essey <joe.essey@salesloft.com>
2018-12-05 22:06:37 -0500

    change to email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     @hockey_game_of_the_day = GameService.best_hockey_of_day
     @basketball_game_of_the_day = GameService.best_basketball_of_day
     @baseball_game_of_the_day = nil
-    @current_user = current_user
+
   end
 
 end
