f7b7f9b3244f4bf2f79e2a462069e099dea9569e
Joe Essey <joe.essey@salesloft.com>
2018-12-11 19:52:54 -0500

    little refactor and polish

--- client/src/layout/AuthenticatedComponent.js
+++ client/src/layout/AuthenticatedComponent.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import FadeIn from 'react-fade-in';
 
 class AuthenticatedComponent extends Component {
 
   state = {
-    data: {}
+    data: {},
+    authenticated: false
   }
 
   componentDidMount() {
     fetch('api/user_profiles/show.json')
       .then(function(response) {
+        if (response.status === 401) {
+          this.setState({ authenticated: false })
+        }
         return response.json();
-      }).then(function(data) {
-        console.log(data);
-        this.setState({ data: data, loading: false });
+      }.bind(this)).then(function(data) {
+        this.setState({ data: data, loading: false, authenticated: true });
       }.bind(this)).catch(function(ex) {
         console.log('parsing failed', ex)
       })
@@ -20,13 +24,13 @@ class AuthenticatedComponent extends Component {
 
   render() {
     return (
-      <div>
-        { this.state.data.team ? (
+      <FadeIn>
+        { this.state.authenticated ? (
           this.state.data.team
         ) : (
           <p>Sorry you can't be here</p>
         )}
-      </div>
+      </FadeIn>
     );
   }
 }
