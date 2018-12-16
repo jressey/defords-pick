73628852198af87387e20d202d9ed4494cad4486
Joe Essey <joe.essey@salesloft.com>
2018-12-16 11:40:42 -0500

    instance vars in game service

--- app/services/game_service.rb
+++ app/services/game_service.rb
@@ -24,29 +24,29 @@ class GameService
   end
 
   def best_football_of_week
-    GameCriteria.by_win_percentage(all_football_for_week)
+    @best_football_of_week || GameCriteria.by_win_percentage(all_football_for_week)
   end
 
   def all_football_for_week
     week_number = NFL::WeekNumberForDate.call(Date.today)
     date_range = NFL::DateForWeekNumber.call(week_number)
-    Game.football.where('start_time BETWEEN ? AND ?', date_range[:start_day], date_range[:end_day]).with_teams
+    @all_football_for_week || Game.football.where('start_time BETWEEN ? AND ?', date_range[:start_day], date_range[:end_day]).with_teams
   end
 
   def best_basketball_of_day
-    GameCriteria.by_win_percentage(Game.basketball.for_day(@offset))
+    @best_basketball_of_day || GameCriteria.by_win_percentage(Game.basketball.for_day(@offset))
   end
 
   def all_basketball_for_day
-    Game.basketball.for_day(@offset).with_teams
+    @all_basketball_for_day || Game.basketball.for_day(@offset).with_teams
   end
 
   def best_hockey_of_day
-    GameCriteria.by_points(Game.hockey.for_day(@offset))
+    @best_hockey_of_day || GameCriteria.by_points(Game.hockey.for_day(@offset))
   end
 
   def all_hockey_for_day
-    Game.hockey.for_day(@offset).with_teams
+    @all_hockey_for_day || Game.hockey.for_day(@offset).with_teams
   end
 
 end
